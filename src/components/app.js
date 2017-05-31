1e32daa1b5d8c334d8fd60e0dbf10f53700e6cef
rimarok <romaric.ruga@gmail.com>
2017-05-31 18:48:44 +0200

    authentication + ajout page

--- src/components/app.js
+++ src/components/app.js
@@ -23,12 +23,20 @@ export default class extends Component {
 
         firebase.initializeApp(firebaseConfig)
 
-        this.state = { data: false, key: null }
+        this.state = {
+            user: null,
+            data: null,
+            key: null
+        }
     }
 
     componentWillMount() {
         
         this.search()
+
+        firebase.auth().onAuthStateChanged(user => this.setState({ user }))
+
+        firebase.auth().getRedirectResult().then(result => this.setState({ user: result.user }))
     }
 
     search(key) {
@@ -51,9 +59,35 @@ export default class extends Component {
         })
     }
 
+    openForm() {
+
+        this.setState({ form: { title: null } })
+    }
+
+    sendForm() {
+        
+        const { key, form: { title }} = this.state
+
+        firebase.database().ref('/datas/'+ key ).set({
+            title,
+            keywords: [],
+            links: [],
+            props: {
+                context: [],
+                inputs: [],
+                outputs: [],
+                functions: []
+            }
+        }).then(() => {
+
+            this.setState({ form: null, key: null })
+            this.search(key)
+        })
+    }
+
     render() {
 
-        const { key, data } = this.state
+        const { user, key, data, form } = this.state
 
         const keywordsContainerParams = {
 
@@ -81,8 +115,6 @@ export default class extends Component {
             return <ItemsContainer { ...params } />
         })
 
-        console.log(this.state)
-
         return (
 
             <div className="app container">
@@ -90,19 +122,50 @@ export default class extends Component {
                 <div className="header row">
 
                     <h2 className="logo four columns">permadata</h2>
-                    <input type="text" className="search eight columns" placeholder="rechercher"
+                    <input type="text" className="search six columns" placeholder="rechercher"
                             onChange={ (e) => this.search(e.target.value) } />
+
+                    {
+                        user ? (
+                            <button type="button" title={ user.displayName }
+                                    onClick={ () => firebase.auth().signOut() }>
+                                deconnexion</button>                            
+                        ) : (
+                            <button type="button"
+                                    onClick={ () => firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider()) }>
+                                connexion</button>
+                        )
+                    }
                 </div>
                 <div className="title">
 
                     { data && <h1>{ data.title }</h1> }
 
-                    { (!data && key) && (
-                        <div>
-                            <p><b>"{ key }"</b> n'existe pas encore</p>
-                            <button type="button">ajouter la page</button>
-                        </div>
-                    )}
+                    { 
+                        (!data && !form && key) && (
+                            <div>
+                                <p><b>"{ key }"</b> n'existe pas encore</p>
+                                <button type="button"
+                                        onClick={ () => this.setState({ form: { title: null }}) }>
+                                    ajouter la page</button>
+                            </div>
+                        )
+                    }
+
+                    {
+                        form && (
+                            <div className="data-form row">
+                                <input type="text" placeholder="titre de la page" className="four columns"
+                                       onChange={ (e) => this.setState({ form: { title: e.target.value }}) } />
+                                <button type="button" className="four columns"
+                                        onClick={ () => this.sendForm() }>
+                                    envoyer</button>
+                                <button type="button" className="four columns"
+                                        onClick={ () => this.setState({ form: null }) }>
+                                    annuler</button>
+                            </div>
+                        )
+                    }
 
                 </div>
                 <div className="content">
